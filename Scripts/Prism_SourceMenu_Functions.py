0036f0ab30fcb0b7d320f1276cdc037acbc56ba4
Joshua Breckeen <86539171+AltaArts@users.noreply.github.com>
2025-03-31 12:23:51 -0400

    Initial Upload

---
+++ Scripts/Prism_SourceMenu_Functions.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+#
+####################################################
+#
+# PRISM - Pipeline for animation and VFX projects
+#
+# www.prism-pipeline.com
+#
+# contact: contact@prism-pipeline.com
+#
+####################################################
+#
+#
+# Copyright (C) 2016-2023 Richard Frangenberg
+# Copyright (C) 2023 Prism Software GmbH
+#
+# Licensed under GNU LGPL-3.0-or-later
+#
+# This file is part of Prism.
+#
+# Prism is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Prism is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Prism.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+import sys
+
+from qtpy.QtCore import *
+from qtpy.QtGui import *
+from qtpy.QtWidgets import *
+
+from PrismUtils.Decorators import err_catcher_plugin as err_catcher
+
+pluginPath = os.path.dirname(os.path.dirname(__file__))                              #   NEEDED ???
+scriptsPath = os.path.join(pluginPath, "Scripts")                   #   NEEDED ???
+
+sys.path.append(pluginPath)
+sys.path.append(scriptsPath)                                        #   NEEDED ???
+sys.path.append(os.path.join(pluginPath, "Libs"))
+
+import SourceBrowser                                                #   NEEDED ???
+
+
+class Prism_SourceMenu_Functions(object):
+    def __init__(self, core, plugin):
+        self.core = core
+        self.plugin = plugin
+
+        #   Callbacks
+        self.core.registerCallback("postInitialize", self.postInitialize, plugin=self)   
+        self.core.registerCallback("onProjectBrowserStartup", self.sourceBrowserStartup, plugin=self)   
+
+
+    # if returns true, the plugin will be loaded by Prism
+    @err_catcher(name=__name__)
+    def isActive(self):
+        return True
+    
+    
+    def sourceBrowserStartup(self, origin):
+
+        self.pbMenu = origin
+
+
+    def postInitialize(self):
+        self.pb = self.core.pb
+        self.sourceBrowser = SourceBrowser.SourceBrowser(core=self.core, projectBrowser=self.pb, refresh=False)
+        self.pbMenu.addTab("Source", self.sourceBrowser, position=0)
+
